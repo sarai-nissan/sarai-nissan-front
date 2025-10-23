f6ab87cb26486b368996f3ca3f233b04c281865c
DemakovAndrii <demakov1155@gmail.com>
2025-10-23 19:59:30 +0100

    add Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { useProductStore } from "./store/productStore";
 import { useEventStore } from "./store/eventStore";
 import { OrderProvider } from "./contexts/OrderContext";
@@ -55,6 +56,7 @@ function App() {
 			<BasketProvider>
 				<OrderProvider>
 					<FilterProvider>
+						<Analytics />
 						<AppRoutes />
 					</FilterProvider>
 				</OrderProvider>
