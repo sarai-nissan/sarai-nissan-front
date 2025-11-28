2e469b202f7ef0e785d2ea6b2b4632f4bb5561c0
DemakovAndrii <demakov1155@gmail.com>
2025-11-28 11:45:54 +0000

    hide save order to back

--- src/pages/confirmation/ConfirmationPage.tsx
+++ src/pages/confirmation/ConfirmationPage.tsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react";
 import { useBasket } from "../../contexts/BasketContext";
-import { ORDER_STORAGE_KEY, useOrder } from "../../contexts/OrderContext";
-import { createOrder } from "../../api";
+import {
+	// ORDER_STORAGE_KEY,
+	useOrder,
+} from "../../contexts/OrderContext";
+// import { createOrder } from "../../api";
 import "./confirmationPage.css";
 
 const ConfirmationPage: React.FC = () => {
@@ -9,33 +12,33 @@ const ConfirmationPage: React.FC = () => {
 	const { clearOrder } = useOrder();
 
 	useEffect(() => {
-		const urlParams = new URLSearchParams(window.location.search);
-		if (urlParams.get("success") !== "true") return;
+		// const urlParams = new URLSearchParams(window.location.search);
+		// if (urlParams.get("success") !== "true") return;
 
-		setTimeout(() => {
-			const saved = JSON.parse(localStorage.getItem(ORDER_STORAGE_KEY) || "{}");
+		// setTimeout(() => {
+		// 	const saved = JSON.parse(localStorage.getItem(ORDER_STORAGE_KEY) || "{}");
 
-			if (saved?.basketItems?.length) {
-				createOrder({
-					email: saved.form.email,
-					phone: saved.form.phone,
-					delivery: saved.form.delivery,
-					firstName: saved.form.firstName,
-					lastName: saved.form.lastName,
-					address1: saved.form.address1,
-					address2: saved.form.address2,
-					city: saved.form.city,
-					state: saved.form.state,
-					postalCode: saved.form.postalCode,
-					country: saved.form.country,
-					basket: saved.basketItems,
-					archived: false,
-				});
-			}
+		// 	if (saved?.basketItems?.length) {
+		// 		createOrder({
+		// 			email: saved.form.email,
+		// 			phone: saved.form.phone,
+		// 			delivery: saved.form.delivery,
+		// 			firstName: saved.form.firstName,
+		// 			lastName: saved.form.lastName,
+		// 			address1: saved.form.address1,
+		// 			address2: saved.form.address2,
+		// 			city: saved.form.city,
+		// 			state: saved.form.state,
+		// 			postalCode: saved.form.postalCode,
+		// 			country: saved.form.country,
+		// 			basket: saved.basketItems,
+		// 			archived: false,
+		// 		});
+		// 	}
 
-			clearBasket();
-			clearOrder();
-		}, 200);
+		clearBasket();
+		clearOrder();
+		// }, 200);
 	}, []);
 
 	return (
